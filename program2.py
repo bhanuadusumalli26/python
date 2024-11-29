3840ef328e6a7765847c523b8aa8632e2421ecce
Bhanu Adusumalli <bhanu.adusumalli11@gmail.com>
2024-11-29 20:35:45 +0530

    Addition

---
+++ program2.py
@@ -0,0 +1,15 @@
+#Addition
+num1=float(input("enter  the first number"))
+num2=float(input("enter the second number"))
+sum=num1+num2
+print(sum)
+
+#division
+num3=float(input("enter  the dividend for division"))
+num4=float(input("enter the divisor for division"))
+if num4==0:
+    print("Error : Divisin  by Zero is not possible")
+else:
+    div_result=num3/num4
+    print(div_result)
+
