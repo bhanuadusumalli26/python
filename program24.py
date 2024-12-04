772ea47595fc562eff3ada8bbb2c67687ba8a22b
Bhanu Adusumalli <bhanu.adusumalli11@gmail.com>
2024-12-04 21:05:15 +0530

    python program to  convert Decimal to Binary,Octal,Hexadecimal

---
+++ program24.py
@@ -0,0 +1,5 @@
+#python program to  convert Decimal to Binary,Octal,Hexadecimal
+dec_num=int(input("Enter  a decimal number :"))
+print("The binary form of ",dec_num,"is :",bin(dec_num))
+print("The octal form of ",dec_num,"is :",oct(dec_num))
+print("The hexadecimal form of ",dec_num,"is :",hex(dec_num))
